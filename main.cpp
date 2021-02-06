50304b939ceaaa62a39f66951f77f078d72a5fd7
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2021-02-07 01:19:53 +0200

    fix startup file, test usb

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
 #include "mbed.h"
 #include "rtos.h"
+#include "USBSerial.h"
 
+//USBHID hid(8, 8);
 BufferedSerial s(PA_2, PA_3, 115200);
+USBSerial u(false);
 
 FileHandle *mbed::mbed_override_console(int fd) {
     return &s;
 }
 
-int main(void) {
+Thread thread;
+
+void usb_thread() {
+	int i = 0;
 	while (true) {
-		printf("Hello?\r\n");
-		ThisThread::sleep_for(1s);
+		u.printf("Hello USB %d\r\n", i++);
+		ThisThread::sleep_for(100ms);
+	}
+}
+
+int main(void) {
+	thread.start(usb_thread);
+	
+	u.set_blocking(false);
+	u.connect();
+	
+	int i = 0;
+	while (true) {
+		printf("Hello? %d\r\n", i++);
+		ThisThread::sleep_for(100ms);
 	}
     return 0;
 }
